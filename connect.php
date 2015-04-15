2fc5fce56b89be0c044c9bea05352e35f1735a1d
Brandon Nguyen <eagleeye2555@gmail.com>
2015-04-15 10:58:02 -0700

    Video 4 of our To-Do App

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+//connects our website to mysqli server
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+//$mysqli->connect_error then we want it to die and have this message
+if ($mysqli->connect_error) { 
+	die('Connect Error (' . $mysqli->connect_error .')'
+		. $mysqli->connect_error);
+}
+else {
+	echo "Connection made";
+}
+$mysqli->close();
+
+?>
